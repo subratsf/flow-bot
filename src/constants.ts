90d120a6bba393c00708712263a426530e052fe5
subrat.thakur <subrat.thakur@salesforce.com>
2021-08-19 23:26:42 +0530

    added urls

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 
 export class Constants {
-    public static readonly SLACK_WEB_HOOK="https://hooks.slack.com/services/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";
+    public static readonly SLACK_WEB_HOOK="https://hooks.slack.com/services/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";
 }
